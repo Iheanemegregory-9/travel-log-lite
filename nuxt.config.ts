87d35d5f3f7d0d88bf4aff36e1d4f3193709f2f3
chibuzor <iheanemegregory9@gmail.com>
2025-07-11 14:16:31 +0100

    added tawilwindcss / daisy ui / icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+
   nitro: {
     prerender: {
       autoSubfolderIndex: false,
     },
   },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["@nuxt/icon"],
 });
